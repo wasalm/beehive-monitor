8fce78fa31fdd9502d8f3da32f00a927829638d4
Andries Salm <info@andries-salm.com>
2020-05-31 19:07:45 +0200

    First commit

---
+++ Arduino/beehive-monitor/beehive-monitor.ino
@@ -0,0 +1,25 @@
+/*
+ * Libaries
+ */
+
+
+/*
+ * Configuration
+ */
+
+/*
+ * Setup Functions
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+/*
+ * Main loop
+ */
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
