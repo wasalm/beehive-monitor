4f20abb38b8598c0d5cfccb09abd64f4a585b528
Andries Salm <info@andries-salm.com>
2020-06-30 18:49:09 +0200

    enabled reset function

--- Arduino/beehive-monitor-v3/beehive-monitor-v3/src/main.cpp
+++ Arduino/beehive-monitor-v3/beehive-monitor-v3/src/main.cpp
@@ -1,25 +1,94 @@
 #include <Arduino.h>
+#include <avr/wdt.h>
 #include <pb_arduino.h>
+#include <cobs/Stream.h>
+#include <cobs/Print.h>
 #include "messages.pb.h"
 
-void setup() {
+using namespace packetio;
+
+COBSStream cobs_in(Serial);
+
+COBSPrint cobs_out(Serial);
+pb_ostream_s pb_out = as_pb_ostream(cobs_out);
+
+void setup()
+{
   Serial.begin(115200);
   // put your setup code here, to run once:
 
   Response response = Response_init_default;
   response.type.resetResponse.dummy_field = 0;
   response.which_type = Response_resetResponse_tag;
-  pb_ostream_s pb_out = as_pb_ostream(Serial);
-  bool status = pb_encode(&pb_out, Response_fields, &response);
-  
-  /* Then just check for any errors.. */
-  // if (!status) {
-      // return ;
-  // }
-
-   
+  pb_encode(&pb_out, Response_fields, &response);
+  cobs_out.end();
+  cobs_in.next();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  uint8_t buffer[255];
+  size_t n = 0;
+
+  while (true)
+  {
+    // read until we get something
+    int c = cobs_in.read();
+    if (c == -1) // End of file
+      continue;
+
+    // detect End Of Packet
+    if (c == -2)
+      break;
+
+    // save anything else
+    buffer[n++] = c;
+
+    if (n == 255)
+      break;
+  }
+
+  Request request = Request_init_zero;
+  Response response = Response_init_default;
+  response.which_type = Response_errorResponse_tag; // Default we want an error message
+
+  pb_istream_t pb_in = pb_istream_from_buffer(buffer, n);
+  bool status = pb_decode(&pb_in, Request_fields, &request);
+
+  /* Check for errors... */
+  if (!status)
+  {
+    // cobs_out.println(PB_GET_ERROR(&pb_in));
+    // cobs_out.end();
+
+    //TODO ERROR STRING
+  }
+  else
+  {
+    switch (request.which_type)
+    {
+    case Request_resetRequest_tag:
+      // Reset device
+      wdt_enable(WDTO_15MS); // Enable watchdog
+      while (1)
+      {};
+      break;
+
+    default:
+      //TODO ERROR STRING
+      break;
+    }
+
+    // ResetRequest resetRequest = 1;
+    // ConfigureRequest configureRequest = 2;
+    // BME280Request bme280Request = 3;
+    // HX711Request hx711Request = 4;
+    // DS18B20Request dS18B20Request = 5;
+    // SSD1315Request ssd1315Request = 6;
+  }
+
+  // start reading the next message
+  pb_encode(&pb_out, Response_fields, &response);
+  cobs_out.end();
+  cobs_in.next();
 }
