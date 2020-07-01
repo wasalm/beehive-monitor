747f44ba5cc869af4f7ccd45eb49f20e7545d843
Andries Salm <info@andries-salm.com>
2020-07-01 20:06:26 +0200

    Added tests for sensors and screens

--- Arduino/beehive-monitor-v3/src/main.cpp
+++ Arduino/beehive-monitor-v3/src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 #include <avr/wdt.h>
 
+// BME280
+#include <U8g2lib.h>
+#include <Wire.h>
+#include <SparkFunBME280.h>
+
+
 char buffer[256]; // Serial buffer
 uint8_t bufPtr = 0;
 
@@ -13,12 +19,41 @@ void softwareReset() {
 void setup() {
   Serial.begin(115200);
   Serial.println("R"); // Device is reset
+
+  //Test
+  BME280 * bme280Ptr = new BME280;
+  bme280Ptr -> setI2CAddress(0x76);
+  if (bme280Ptr -> beginI2C() == false) {
+    Serial.println("The sensor did not respond. Please check wiring.");
+    while(1); //Freeze
+  }
+
+  Serial.println("AA"); 
+
+  U8X8_SSD1306_128X64_NONAME_SW_I2C * u8x8;
+  u8x8 = new U8X8_SSD1306_128X64_NONAME_SW_I2C(A2, A3, U8X8_PIN_NONE);
+  //(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
+  
+
+  Serial.println("AB"); 
+  u8x8 -> begin();
+
+  Serial.println("BB"); 
+
+  u8x8 -> clearDisplay();         // clear the internal memory
+  //draw some you want; such as 
+  u8x8 -> setFont(u8x8_font_amstrad_cpc_extended_f);
+  u8x8 -> drawString(0,10,"Hello World!");  // write something to the internal memory
+
+  Serial.println("CC"); 
+
+
 }
 
 void parseMessage() {
   switch (buffer[0])
   {
-  case "R:":
+  case 'R':
     softwareReset();
     break;
   
